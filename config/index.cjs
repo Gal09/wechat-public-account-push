a38e76baea05ea9d3cd589cc4d80205fb903348c
Gal09 <115346525+Gal09@users.noreply.github.com>
2022-10-08 23:37:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'e2903afab0bbb3f310217008251f5b51',
+      id: 'oAOGK6FeLXnr2_O7ImxtEFwWGQKA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'MoGJNuBlVKetafxTiCT7KSsPjZMeNM56nUg0TFkTbVE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
